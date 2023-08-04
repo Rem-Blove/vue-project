cbd332325bcc600bd1e979a957cea451aad6b290
Rem <1941398048@qq.com>
2023-08-04 18:20:52 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        htmlWhitespaceSensitivity: 'ignore',
+        trailingComma: 'none',
+        semi: false,
+        singleQuote: true,
+        arrowParens: 'avoid',
+        bracketSameLine: false
+      }
+    ],
+    '@typescript-eslint/promise-function-async': 'off',
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/space-before-function-paren': 0
+  }
+}
